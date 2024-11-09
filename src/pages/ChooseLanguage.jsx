c6341a3308e0b0272db1bce5d30e618002a0a132
choiyeon <2dpaesu@gmail.com>
2024-11-09 14:05:04 +0900

    [Style] code fomating

--- src/pages/ChooseLanguage.jsx
+++ src/pages/ChooseLanguage.jsx
@@ -2,7 +2,6 @@ import GoBack from '../components/GoBack';
 import CharacterItem from '../components/CharacterItem';
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { useMemo } from 'react';
 import axios from 'axios';
 import IMAGES from "../images/images";
 import '../App.css';
