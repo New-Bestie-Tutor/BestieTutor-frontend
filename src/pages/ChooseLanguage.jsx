b0584b079733e84f049045de721c361446ad7914
{joonwooHeo} <{hensher5050@gmail.com}>
2025-05-08 20:19:32 +0900

    [feat] 진행바 추가

--- src/pages/ChooseLanguage.jsx
+++ src/pages/ChooseLanguage.jsx
@@ -2,6 +2,7 @@ import GoBack from '../components/GoBack';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import IMAGES from "../images/images";
+import ProgressBar from '../components/ProgressBar';
 import '../App.css';
 
 export default function ChooseLanguage() {
@@ -12,32 +13,35 @@ export default function ChooseLanguage() {
 
     const handleNextSurvey = () => {
         if (selectedLanguage) {
-            navigate('/learningGoal', { state: { language: selectedLanguage } }); 
+            navigate('/learningGoal', { state: { language: selectedLanguage } });
         }
     };
-    
+
     return (
         <div className="topic">
-            <GoBack className='goBack'/> 
-            <p className="conversation-title">배울 언어를 선택해주세요</p>
-
-            <div className='character-wrapper'>
+            <GoBack className='goBack' />
+            <p className="conversation-title">내가 배우고 싶은 언어를 선택해주세요</p>
+            <ProgressBar />
+            <div className='language-wrapper'>
                 <div className="language-list">
                     {languages.map((language, index) => (
-                    <div 
-                        key={index}
-                        className='language-item' 
-                        style={{ backgroundColor: selectedLanguage === language ? '#EBFFEE' : '#FAFAFC' }} 
-                        onClick={() => setSelectedLanguage(language)}
-                    >
-                        <img src={IMAGES[language]} alt={language} className="character-icon" />
-                        <p className="character-text">{language}</p>
-                    </div>
+                        <div
+                            key={index}
+                            className='language-item'
+                            style={{
+                                backgroundColor: selectedLanguage === language ? '#EBFFEE' : '#FAFAFC',
+                                border: selectedLanguage === language ? '2px solid #00C471' : 'none'
+                            }}
+                            onClick={() => setSelectedLanguage(language)}
+                        >
+                            <img src={IMAGES[language]} alt={language} className="character-icon" />
+                            <p className="character-text">{language}</p>
+                        </div>
                     ))}
                 </div>
             </div>
-            <button 
-                className="next-button" 
+            <button
+                className="next-button"
                 onClick={handleNextSurvey}
             >
                 다음
--- src/pages/PreferredTopic.jsx
+++ src/pages/PreferredTopic.jsx
@@ -5,6 +5,7 @@ import { UserContext } from "../UserContext";
 import '../App.css';
 import axios from 'axios';
 import IMAGES from "../images/images";
+import ProgressBar from '../components/ProgressBar';
 
 export default function PreferredTopic() {
     const navigate = useNavigate();
@@ -13,13 +14,13 @@ export default function PreferredTopic() {
     const location = useLocation();
     const { language, goals, level } = location.state;
     const [selectedTopics, setSelectedTopics] = useState([]);
-    const topics = ['일상','비즈니스','취미','여행'];
-    
+    const topics = ['일상', '비즈니스', '취미', '여행'];
+
     const handleNextSurvey = async () => {
         if (selectedTopics.length >= 1 && selectedTopics.length <= 3) {// 목표 3개까지 선택 가능
             const result = await preference();
             if (result && result.message === '선호도 조사 완료') {
-                navigate('/home'); 
+                navigate('/home');
             } else {
                 // console.error("선호도 조사에 문제가 발생했습니다.");
             }
@@ -28,23 +29,23 @@ export default function PreferredTopic() {
 
     async function preference() {
         const userData = {
-            userId: userId, 
+            userId: userId,
             language: language,
             learningGoals: goals,
             preferredTopics: selectedTopics,
             currentSkillLevel: level
         };
-    
-        const response = await axios.post('/preference', userData); 
+
+        const response = await axios.post('/preference', userData);
         return response.data;
-      }
+    }
 
     const handleTopicClick = (topic) => {
         setSelectedTopics((prev) => {
-            if(prev.includes(topic)){
+            if (prev.includes(topic)) {
                 //목표가 이미 선택된 경우, 선택 해제
                 return prev.filter((item) => item !== topic);
-            } else if (prev.length < 3){
+            } else if (prev.length < 3) {
                 return [...prev, topic];
             }
             return prev;
@@ -53,29 +54,30 @@ export default function PreferredTopic() {
 
     return (
         <div className="topic">
-            <GoBack className='goBack'/> 
+            <GoBack className='goBack' />
             <p className="conversation-title">관심있는 분야를 선택해주세요
-            <span style={{ fontSize: '15px', color: 'rgba(0, 0, 0, 0.36)' }}>
-                (최대 3개)
-            </span>
+                <ProgressBar />
+                <span style={{ fontSize: '15px', color: 'rgba(0, 0, 0, 0.36)' }}>
+                    (최대 3개)
+                </span>
             </p>
             <div className='character-wrapper'>
                 <div className="goal-list">
                     {topics.map((topic, index) => (
-                    <div 
-                        key={index}
-                        className='preferredTopic-item' 
-                        style={{ backgroundColor: selectedTopics.includes(topic) ? '#EBFFEE' : '#FAFAFC' }}
-                        onClick={() => handleTopicClick(topic)}
-                    >
-                        <img src={IMAGES[topic]} alt={topic} className="preferredTopic-icon" />
-                        <p className="goal-text">{topic}</p>
-                    </div>
+                        <div
+                            key={index}
+                            className='preferredTopic-item'
+                            style={{ backgroundColor: selectedTopics.includes(topic) ? '#EBFFEE' : '#FAFAFC' }}
+                            onClick={() => handleTopicClick(topic)}
+                        >
+                            <img src={IMAGES[topic]} alt={topic} className="preferredTopic-icon" />
+                            <p className="goal-text">{topic}</p>
+                        </div>
                     ))}
                 </div>
             </div>
-            <button 
-                className="next-button" 
+            <button
+                className="next-button"
                 onClick={handleNextSurvey}
             >
                 다음
